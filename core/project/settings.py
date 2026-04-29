6e22fb7bd779a2024f834859465e470e475668b5
Alazar Bekele <alazarthe70@gmail.com>
2026-04-29 23:24:45 +0300

    fix: Change Allowed host

--- core/project/settings.py
+++ core/project/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y@l2=*kh+a(54vlo8cpjut#=j0%o=2&y%%z4g%78*o)vzoo^n)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
